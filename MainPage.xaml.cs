354a86b36f35c6b6be9c679391bbea0d91ee3ea8
Nico Zhu <v-zhumin@microsoft.com>
2018-01-25 18:09:13 +0800

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using SwipeControlTest.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Windows.Input;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SwipeControlTest
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ViewModel viewmodel { get; set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = new ViewModel();
+            viewmodel = this.DataContext as ViewModel;
+        }
+
+        private int i = 0;
+
+        private void UpdateCountCommand_CanExecuteChanged(object sender, EventArgs e)
+        {
+            Debug.WriteLine(i++);
+        }
+
+        public ObservableCollection<SampleModel> list = new ObservableCollection<SampleModel>();
+
+        private void DeleteItem_ItemInvoked(SwipeItem sender, SwipeItemInvokedEventArgs args)
+        {
+        }
+
+        private void Flag_ItemInvoked(SwipeItem sender, SwipeItemInvokedEventArgs args)
+        {
+        }
+
+        private void Accept_ItemInvoked(SwipeItem sender, SwipeItemInvokedEventArgs args)
+        {
+        }
+
+        private void swipe_Invoked(SwipeItem sender, SwipeItemInvokedEventArgs args)
+        {
+        }
+    }
+}
---
+++ ViewModels/SampleModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SwipeControlTest.ViewModels
+{
+    public class ViewModel
+    {
+        public ObservableCollection<SampleModel> list { get; set; } = new ObservableCollection<SampleModel>();
+        public ICommand MyCommand { get; set; }
+
+        private Action<object> action = (paramet) =>
+         {
+             Debug.WriteLine(paramet.GetType());
+         };
+
+        public ViewModel()
+        {
+            list.Add(new SampleModel() { Text = "Hello1" });
+            list.Add(new SampleModel() { Text = "Hello2" });
+            list.Add(new SampleModel() { Text = "Hello3" });
+            list.Add(new SampleModel() { Text = "Hello4" });
+            list.Add(new SampleModel() { Text = "Hello5" });
+            list.Add(new SampleModel() { Text = "Hello6" });
+            list.Add(new SampleModel() { Text = "Hello7" });
+            list.Add(new SampleModel() { Text = "Hello8" });
+            list.Add(new SampleModel() { Text = "Hello9" });
+            list.Add(new SampleModel() { Text = "Hello10" });
+            MyCommand = new SampleCommand(action, true);
+        }
+    }
+
+    public class SampleModel
+    {
+        public string Text { get; set; }
+
+        public ICommand IncreaseItemCommand { get; set; }
+    }
+
+    public class SampleCommand : ICommand
+    {
+        public SampleCommand(Action<object> action, bool canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        private Action<object> _action;
+        private bool _canExecute;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action(parameter);
+        }
+    }
+}
